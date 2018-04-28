ccdb0da7e981bb78de4706cddae818f274c8ea03
Angela Shen <angelashen@angelas-mbp.lan>
2018-04-28 00:07:00 -0700

    Write code to download images

--- wiki-scraper/wiki-scraper.py
+++ wiki-scraper/wiki-scraper.py
@@ -1,49 +1,62 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import os
+ 
+artworks_list = []
+counter = 1
 
-artist_list = [];  
-
-with open("artist-list.txt") as in_file:
-    for line in in_file:
-        line = line.strip("\n")
-        artist_list.append(line)
-
+def main():
+    with open("artist-list.txt") as in_file:
+        for line in in_file:
+            artist = line.strip("\n")
+            get_artworks(artist)
 
 def get_artworks(artist):
-    paintings_list = [];
+    global counter
+    # acceptable file extensions
     file_extensions = ['jpg', 'jpeg', 'png']
     r = requests.get('https://en.wikipedia.org/wiki/' + artist)
     soup = BeautifulSoup(r.text, "html.parser")
     # get all anchor tags with class="image"
     anchor_tags = soup.select("a[class=image]")
     for a in anchor_tags:
+        artwork_dict = {}
+        # get src from image tag nested inside anchor tags
+        src = a.select("img")[0].get('src')
+        # resize all images 650px
+        src = re.sub(r'[0-9]*px', "650px", src).strip("//")
         href = a.get('href')
         if href[-3:] in file_extensions:
            title = get_img_title(href)
            if title != None:
-                print(title)
-
-# get title of image
+                n = 4 - len(str(counter))
+                img_id = 'img' + n*str(0) + str(counter)
+                artwork_dict["img_id"] = img_id
+                artwork_dict["title"] = title
+                artwork_dict["artist"] = artist
+                artwork_dict["is_easy"] = False
+                artworks_list.append(artwork_dict)
+                counter += 1
+                download_img(img_id, src)
+                
+# get title of image from wikipedia
 def get_img_title(href):
     r = requests.get('https://en.wikipedia.org' + href)
-    soup = BeautifulSoup(r.text, "html.parser")
+    soup = BeautifulSoup(r.text, "html.parser") 
     title_elt = soup.select("td[id=fileinfotpl_art_title] + td > span")
-    artist_elt = soup.select("td[id=fileinfotpl_aut] + td > div > table > tr > th > span")
+    #artist_elt = soup.select("td[id=fileinfotpl_aut] + td > div > table > tr > th > span")
     if len(title_elt) > 0:
         return title_elt[0].text
 
-    # to do 
+# downloads image from url and stores in artworks folder 
+def download_img(img_id, url):
+    img_data = requests.get('http://' + url).content
+    img_path = './artworks/' + img_id + '.jpg'
+    with open(img_path, 'wb+') as handler:
+        handler.write(img_data)
+    print(img_id + " successfully downloaded")
 
-get_artworks('Paul Cezanne')
-# https://tools.wmflabs.org/magnus-toolserver/commonsapi.php?image=
-#get_artworks("Leonardo Da Vinci")
 
-# {'Vincent Van Gogh': [  {'title': 'Starry Night', 'url': 'url.com'},
-#                         {'title': 'Sunflowers', 'url': 'url.com'},
-#                         {'title': 'Windmills', 'url': 'url.com'} ],
-#  "Georgia O'Keefe": [
-#                                                     ]
-# }
-
-#img['src'] = re.sub(r'[0-9]*px', "512px", img['src']).strip("//")
+if __name__ == "__main__":
+    main()
